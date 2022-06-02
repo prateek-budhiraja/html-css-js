0b55405b3aba0fa335f88686388e4244b88da2a0
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2022-06-02 10:52:28 +0530

    unclicable button project

---
+++ unclickable/script.js
@@ -0,0 +1,8 @@
+var width = window.innerWidth - 100;
+var height = window.innerHeight - 50;
+
+const btn = document.getElementsByClassName("button");
+btn[0].addEventListener("mouseover", () => {
+  btn[0].style.top = Math.floor(Math.random() * width) + "px";
+  btn[0].style.left = Math.floor(Math.random() * height) + "px";
+});
