a5fc0a2942bea8f3abf5343a5f722a405029c799
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2022-06-11 21:35:39 +0530

    added styles and transition, changed names

--- toggle-button-1/script.js
+++ toggle-button-1/script.js
@@ -1,9 +1,10 @@
-const toggle = document.querySelector(".container");
-const button = document.querySelector(".button");
+const toggle = document.querySelector(".toggle-container");
+const button = document.querySelector(".toggle-button");
 const position = button.style.left;
 
 toggle.addEventListener("click", changeToggleState);
 
 function changeToggleState() {
 	button.classList.toggle("active");
+	toggle.classList.toggle("active-container");
 }
